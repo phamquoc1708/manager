23ceb6d0df86cd3abcf95ad66d0007fde3dd1c27
phamquoc <quoc.pham@c2c-techhub.io>
2023-05-26 22:27:46 +0700

    first commit

---
+++ src/layouts/dashboard/config.js
@@ -0,0 +1,36 @@
+import CogIcon from "@heroicons/react/24/solid/CogIcon";
+import UserIcon from "@heroicons/react/24/solid/UserIcon";
+import UserPlusIcon from "@heroicons/react/24/solid/UserPlusIcon";
+import UsersIcon from "@heroicons/react/24/solid/UsersIcon";
+import XCircleIcon from "@heroicons/react/24/solid/XCircleIcon";
+import { SvgIcon } from "@mui/material";
+
+export const items = [
+  {
+    title: "Quản lý vườn",
+    path: "/customers",
+    icon: (
+      <SvgIcon fontSize="small">
+        <UsersIcon />
+      </SvgIcon>
+    ),
+  },
+  {
+    title: "Tài khoản",
+    path: "/account",
+    icon: (
+      <SvgIcon fontSize="small">
+        <UserIcon />
+      </SvgIcon>
+    ),
+  },
+  {
+    title: "Cài đặt",
+    path: "/settings",
+    icon: (
+      <SvgIcon fontSize="small">
+        <CogIcon />
+      </SvgIcon>
+    ),
+  },
+];
---
+++ src/pages/auth/login.js
@@ -0,0 +1,143 @@
+import { useCallback, useState } from "react";
+import Head from "next/head";
+import NextLink from "next/link";
+import { useRouter } from "next/navigation";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import {
+  Alert,
+  Box,
+  Button,
+  FormHelperText,
+  Link,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useAuth } from "src/hooks/use-auth";
+import { Layout as AuthLayout } from "src/layouts/auth/layout";
+
+const Page = () => {
+  const router = useRouter();
+  const auth = useAuth();
+  const [method, setMethod] = useState("email");
+  const formik = useFormik({
+    initialValues: {
+      email: "demo@devias.io",
+      password: "Password123!",
+      submit: null,
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Must be a valid email")
+        .max(255)
+        .required("Email is required"),
+      password: Yup.string().max(255).required("Password is required"),
+    }),
+    onSubmit: async (values, helpers) => {
+      try {
+        await auth.signIn(values.email, values.password);
+        router.push("/");
+      } catch (err) {
+        helpers.setStatus({ success: false });
+        helpers.setErrors({ submit: err.message });
+        helpers.setSubmitting(false);
+      }
+    },
+  });
+
+  const handleSkip = useCallback(() => {
+    auth.skip();
+    router.push("/");
+  }, [auth, router]);
+
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: "background.paper",
+          flex: "1 1 auto",
+          alignItems: "center",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 550,
+            px: 3,
+            py: "160px",
+            width: "100%",
+          }}
+        >
+          <div>
+            <Stack spacing={1} sx={{ mb: 3 }}>
+              <Typography variant="h4">Đăng Nhập</Typography>
+              <Typography color="text.secondary" variant="body2">
+                Nếu bạn chưa có tài khoản? &nbsp;
+                <Link
+                  component={NextLink}
+                  href="/auth/register"
+                  underline="hover"
+                  variant="subtitle2"
+                >
+                  Đăng ký
+                </Link>
+              </Typography>
+            </Stack>
+            <form noValidate onSubmit={formik.handleSubmit}>
+              <Stack spacing={3}>
+                <TextField
+                  error={!!(formik.touched.email && formik.errors.email)}
+                  fullWidth
+                  helperText={formik.touched.email && formik.errors.email}
+                  label="Địa chỉ Email"
+                  name="email"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  type="email"
+                  value={formik.values.email}
+                />
+                <TextField
+                  error={!!(formik.touched.password && formik.errors.password)}
+                  fullWidth
+                  helperText={formik.touched.password && formik.errors.password}
+                  label="Mật khẩu"
+                  name="password"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  type="password"
+                  value={formik.values.password}
+                />
+              </Stack>
+              {formik.errors.submit && (
+                <Typography color="error" sx={{ mt: 3 }} variant="body2">
+                  {formik.errors.submit}
+                </Typography>
+              )}
+              <Button
+                fullWidth
+                size="large"
+                sx={{ mt: 3 }}
+                type="submit"
+                variant="contained"
+              >
+                Đăng Nhập
+              </Button>
+              <Alert color="primary" severity="info" sx={{ mt: 3 }}>
+                <div>
+                  Bạn có thể sử dụng <b>demo@devias.io</b> và Mật khẩu{" "}
+                  <b>Password123!</b>
+                </div>
+              </Alert>
+            </form>
+          </div>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+Page.getLayout = (page) => <AuthLayout>{page}</AuthLayout>;
+
+export default Page;
